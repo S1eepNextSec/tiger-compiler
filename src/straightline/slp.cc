a0b9adb02e4bdf751fd654a2ad082de022be7b74
Nurtibbar <1842811232@qq.com>
2024-09-21 12:31:00 +0800

    finish lab1

--- src/straightline/slp.cc
+++ src/straightline/slp.cc
@@ -5,28 +5,117 @@
 namespace A {
 int A::CompoundStm::MaxArgs() const {
   // TODO: put your code here (lab1).
+  int stm1args = stm1->MaxArgs();
+  int stm2args = stm2->MaxArgs();
+
+  return stm1args > stm2args ? stm1args : stm2args;
 }
 
 Table *A::CompoundStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
+  return stm2->Interp(stm1->Interp(t));
 }
 
 int A::AssignStm::MaxArgs() const {
   // TODO: put your code here (lab1).
+  return this->exp->MaxArgs();
 }
 
 Table *A::AssignStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
+  IntAndTable *it = exp->Interp(t);
+
+  return it->t->Update(this->id, it->i);
 }
 
 int A::PrintStm::MaxArgs() const {
   // TODO: put your code here (lab1).
+  return exps->MaxArgs();
 }
 
 Table *A::PrintStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
+  IntAndTable *it = this->exps->Interp(t);
+  return it->t;
 }
 
+int Exp::MaxArgs() const {
+    return 0;
+}
+
+IntAndTable* IdExp::Interp(Table *t) const{
+  int val = t->Lookup(this->id);
+  return new IntAndTable(val, t);
+}
+
+IntAndTable* NumExp::Interp(Table *t) const{
+  return new IntAndTable(this->num, t);
+}
+
+IntAndTable* OpExp::Interp(Table *t) const{
+    IntAndTable* it1 = this->left->Interp(t);
+    IntAndTable* it2 = this->right->Interp(it1->t);
+
+    int val = 0;
+    switch (this->oper) {
+    case PLUS:
+        val = it1->i + it2->i;
+        break;
+    case MINUS:
+        val = it1->i - it2->i;
+        break;
+    case TIMES:
+        val = it1->i * it2->i;
+        break;
+    case DIV:
+        val = it1->i / it2->i;
+        break;
+    }
+    return new IntAndTable(val, it2->t);
+}
+
+IntAndTable* EseqExp::Interp(Table *t) const{
+    Table *t1 = this->stm->Interp(t);
+
+    IntAndTable* it = this->exp->Interp(t1);
+
+    return it;
+}
+
+int EseqExp::MaxArgs() const {
+    int stmMaxArgs = stm->MaxArgs();
+    int expMaxArgs = exp->MaxArgs();
+
+    return stmMaxArgs > expMaxArgs ? stmMaxArgs : expMaxArgs;
+}
+
+int ExpList::MaxArgs() const {
+    return 0;
+}
+
+int PairExpList::MaxArgs() const {
+    return 1 + tail->MaxArgs();
+}
+
+int LastExpList::MaxArgs() const {
+    return 1;
+}
+
+IntAndTable* PairExpList::Interp(Table * t)const{
+    IntAndTable *it1 = this->exp->Interp(t);
+
+    printf("%d ",it1->i);
+
+    IntAndTable *it2 = this->tail->Interp(it1->t);
+    return it2;
+}
+
+IntAndTable* LastExpList::Interp(Table* t)const{
+    IntAndTable *it = this->exp->Interp(t);
+
+    printf("%d\n",it->i);
+    return it;
+}
 
 int Table::Lookup(const std::string &key) const {
   if (id == key) {
--- src/straightline/slp.h
+++ src/straightline/slp.h
@@ -59,14 +59,18 @@ class Exp {
   // TODO: you'll have to add some definitions here (lab1).
   // Hints: You may add interfaces like `int MaxArgs()`,
   //        and ` IntAndTable *Interp(Table *)`
+  public:
+    virtual int MaxArgs() const;
+    virtual IntAndTable * Interp(Table *) const = 0;
 };
 
 class IdExp : public Exp {
  public:
   explicit IdExp(std::string id) : id(std::move(id)) {}
   // TODO: you'll have to add some definitions here (lab1).
+  IntAndTable *Interp(Table *) const override;
 
- private:
+  private:
   std::string id;
 };
 
@@ -74,8 +78,9 @@ class NumExp : public Exp {
  public:
   explicit NumExp(int num) : num(num) {}
   // TODO: you'll have to add some definitions here.
+  IntAndTable *Interp(Table *) const override;
 
- private:
+  private:
   int num;
 };
 
@@ -83,8 +88,9 @@ class OpExp : public Exp {
  public:
   OpExp(Exp *left, BinOp oper, Exp *right)
       : left(left), oper(oper), right(right) {}
+  IntAndTable *Interp(Table *)const override;
 
- private:
+  private:
   Exp *left;
   BinOp oper;
   Exp *right;
@@ -93,8 +99,10 @@ class OpExp : public Exp {
 class EseqExp : public Exp {
  public:
   EseqExp(Stm *stm, Exp *exp) : stm(stm), exp(exp) {}
+  int MaxArgs() const override;
+  IntAndTable *Interp(Table *) const override;
 
- private:
+  private:
   Stm *stm;
   Exp *exp;
 };
@@ -104,13 +112,18 @@ class ExpList {
   // TODO: you'll have to add some definitions here (lab1).
   // Hints: You may add interfaces like `int MaxArgs()`, `int NumExps()`,
   //        and ` IntAndTable *Interp(Table *)`
+    virtual int MaxArgs() const = 0;
+    virtual IntAndTable *Interp(Table *) const = 0;
 };
 
 class PairExpList : public ExpList {
  public:
   PairExpList(Exp *exp, ExpList *tail) : exp(exp), tail(tail) {}
   // TODO: you'll have to add some definitions here (lab1).
- private:
+  IntAndTable *Interp(Table *) const override;
+  int MaxArgs() const override;
+
+  private:
   Exp *exp;
   ExpList *tail;
 };
@@ -119,7 +132,11 @@ class LastExpList : public ExpList {
  public:
   LastExpList(Exp *exp) : exp(exp) {}
   // TODO: you'll have to add some definitions here (lab1).
- private:
+
+  IntAndTable *Interp(Table *) const override;
+  int MaxArgs() const override;
+
+  private:
   Exp *exp;
 };
 
